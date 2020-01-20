9390e6e6925fd0287b26253e69844a49272db5df
tb440167 <terryblack001@gmail.com>
2020-01-20 13:32:46 +0000

    added CORS to app for front end deployment

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const { handle500s, handle404s, handle400s, customErrors, handle422s } = require('./errors');
 const apiRouter = require('./routers/apiRouter');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 
 app.use(handle400s);
 app.use(customErrors);
-app.use(handle422s)
+app.use(handle422s);
 app.all('/*', handle404s);
 app.use(handle500s);
 
